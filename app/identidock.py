c4b45d61d615ea6b1a01c36fb51a957915be51ab
mkone112 <mkone112@gmail.com>
2022-08-19 03:00:38 +0300

    add cache

--- app/identidock.py
+++ app/identidock.py
@@ -1,12 +1,15 @@
 import os
 
-from flask import Flask, Response, render_template, request
 import httpx
+from flask import Flask, Response, render_template, request
+import redis
 
 from const import default_name
 from utils import get_hash
 
 app = Flask(__name__)
+
+cache = redis.StrictRedis('redis')
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -20,11 +23,15 @@ def mainpage():
 
 @app.route('/monster/<name>')
 def get_identicon(name):
-    identicon_api = os.getenv('IDENTICON_API')
-    identicon_size = os.getenv('IDENTICON_SIZE')
-    identicon_url = f'{identicon_api}/monster/{name}?size={identicon_size}'
-    request = httpx.get(identicon_url)
-    image = request.content
+    image = cache.get(name)
+    if image is None:
+        app.logger.info("Cache miss")
+        identicon_api = os.getenv('IDENTICON_API')
+        identicon_size = os.getenv('IDENTICON_SIZE')
+        identicon_url = f'{identicon_api}/monster/{name}?size={identicon_size}'
+        req = httpx.get(identicon_url)
+        image = req.content
+        cache.set(name, image)
 
     return Response(image, mimetype='image/png')
 
